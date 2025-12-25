e1e48fedb45847226fb91a7d5b10e641a73823e7
Gautam Chebrolu <varpra1@gmail.com>
2025-12-24 22:27:09 -0500

    Massive overhaul

---
+++ projects.js
@@ -0,0 +1,84 @@
+// projects.js
+const projectsData = [
+        {
+        title: "Pilleve Dispenser",
+        role: "CTO",
+        year: "2017-2023",
+        category: "hardware design software visuals",
+        image: "https://lh3.googleusercontent.com/pw/AP1GczP7sm87uNlGUrSIu34LuyAAbLs9vMDv7-uPy7KagAmR31N-AbgOpd-PCEALK4vdZWnheXy9wWhFFEd8ea_grqEMoTJxg0_oNZrS69pWfgYVhpf2Fn0aqs2gQPCes73VK-sC4WvaMqmXFtkqeto5gDVv=w2542-h1680-s-no-gm?authuser=1",
+        link: "https://gauta.me"
+    },
+    {
+        title: "SPARQ AI",
+        role: "Hardware Engineer",
+        year: "2024",
+        // Space-separated categories: design, hardware, software, visuals
+        category: "design software", 
+        image: "https://i.kickstarter.com/assets/041/516/682/e5c0cb5bbfcfb086a9180cdec7443e07_original.png?anim=false&fit=cover&gravity=auto&height=873&origin=ugc&q=92&v=1688574831&width=1552&sig=YuOxgLELbFtboI%2FaoPSzSTqG1RdY%2F967EFyYxIzNwPY%3D",
+        link: "https://gauta.me"
+    },
+    {
+        title: "Pro Headshots",
+        role: "Photographer",
+        year: "2019 - Present",
+        category: "visuals",
+        image: "images/project3.jpg",
+        link: "/internal-page.html"
+    },
+    {
+        title: "The Kelloggian",
+        role: "Puzzle Master",
+        year: "2026",
+        category: "design software", 
+        image: "",
+        link: "https://gauta.me"
+    },
+    {
+        title: "Concert Photography",
+        role: "Photographer",
+        year: "2024 - Present",
+        category: "visuals",
+        image: "images/project2.jpg",
+        link: "https://external-site.com/project2"
+    },
+    {
+        title: "Event Photography",
+        role: "Photographer",
+        year: "2015 - Present",
+        category: "visuals",
+        image: "images/project3.jpg",
+        link: "/internal-page.html"
+    },
+        {
+        title: "Pro Headshots",
+        role: "Photographer",
+        year: "2019 - Present",
+        category: "visuals",
+        image: "images/project3.jpg",
+        link: "/internal-page.html"
+    },
+    {
+        title: "The Kelloggian",
+        role: "Puzzle Master",
+        year: "2026",
+        category: "design software", 
+        image: "",
+        link: "https://gauta.me"
+    },
+    {
+        title: "Concert Photography",
+        role: "Photographer",
+        year: "2024 - Present",
+        category: "visuals",
+        image: "images/project2.jpg",
+        link: "https://external-site.com/project2"
+    },
+    {
+        title: "Event Photography",
+        role: "Photographer",
+        year: "2015 - Present",
+        category: "visuals",
+        image: "images/project3.jpg",
+        link: "/internal-page.html"
+    },
+];
